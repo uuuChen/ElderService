3215dd70766805481aac2af22676d11e9985b4af
uuuChen <fredusr1@gmail.com>
2022-03-20 02:41:25 +0800

    feat: add the search_photos() in UnsplashRequester

--- src/image/request.py
+++ src/image/request.py
@@ -1,10 +1,21 @@
 import os
+from src.utils import send_request
 
 
 class UnsplashRequester:
     def __init__(self):
         self._base_url = 'https://api.unsplash.com'
-        self._id = os.environ['UNSPLASH_CLIENT_ID']
 
-    def search(self):
-        pass
+    @staticmethod
+    def _get_auth_header():
+        return {'Authorization': f'Client-ID {os.environ["UNSPLASH_CLIENT_ID"]}'}
+
+    def search_photos(self, query: str, page: int = 1, per_page: int = 10):
+        url = os.path.join(self._base_url, 'search', 'photos')
+        headers = self._get_auth_header()
+        params = {
+            'query': query,
+            'page': page,
+            'per_page': per_page,
+        }
+        return send_request(url, "GET", headers=headers, params=params)
